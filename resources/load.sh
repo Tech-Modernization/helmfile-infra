8e49467238f564b6f24570b4682b55db1b0970b2
Bill Hood <bhood4@gmail.com>
2020-05-07 15:02:06 -0400

    load dashboard cm to prometheus ns, add external additional scapes

--- resources/load.sh
+++ resources/load.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-kubectl create configmap $1 --namespace=myapp-prometheus --from-file=./$1/ --dry-run=true -o yaml | kubectl apply -f -
-kubectl label configmap $1 --namespace=myapp-prometheus --overwrite=true grafana_dashboard="1"
+#kubectl create configmap $1 --namespace=myapp-prometheus --from-file=./$1/ --dry-run=true -o yaml | kubectl apply -f -
+#kubectl label configmap $1 --namespace=myapp-prometheus --overwrite=true grafana_dashboard="1"
+
+kubectl create configmap $1 --namespace=prometheus --from-file=./$1/ --dry-run=true -o yaml | kubectl apply -f -
+kubectl label configmap $1 --namespace=prometheus --overwrite=true grafana_dashboard="1"
