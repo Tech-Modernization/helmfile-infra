f74e1e0b91a41c8c77f7137fd90f6ff6d8d14470
Bill Hood <bhood4@gmail.com>
2020-05-12 21:52:03 -0400

    remove temp certs, probably should do at end as well

--- tls/setup-etcd.sh
+++ tls/setup-etcd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -f certs/*
 gcloud kms keyrings create vault --location global
 gcloud kms keys create etcd --location global --keyring vault --purpose encryption
 gcloud kms keys create init --location global --keyring vault --purpose encryption
