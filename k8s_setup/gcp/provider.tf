32318a1944a577cf93b6862a31baf0536f19bc5d
Bill Hood <bhood4@gmail.com>
2020-06-26 17:47:02 -0400

    module.gke

--- k8s_setup/gcp/provider.tf
+++ k8s_setup/gcp/provider.tf
@@ -7,8 +7,8 @@ provider "kubernetes" {
 #  host = "https://${data.google_container_cluster.primary.endpoint}"
 #  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 
-  host = "https://${google_container_cluster.primary.endpoint}"
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  host = "https://${module.gke.endpoint}"
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 
   token = data.google_client_config.current.access_token
 }
