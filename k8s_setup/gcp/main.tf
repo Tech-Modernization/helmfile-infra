131c1769c2c9a1d9944ec6ddf22bdbf687ad5ee9
Bill Hood <bhood4@gmail.com>
2020-06-26 17:58:16 -0400

    "1.2.3.4/32"

--- k8s_setup/gcp/main.tf
+++ k8s_setup/gcp/main.tf
@@ -50,8 +50,10 @@ module "gke" {
   regional                          = true
   network                           = module.gke-network.network_name
   subnetwork                        = module.gke-network.subnets_names[0]
-  #ip_range_pods                     = module.gke-network.subnets_secondary_ranges[0].*.range_name[0]
-  #ip_range_services                 = module.gke-network.subnets_secondary_ranges[0].*.range_name[1]
+  ip_range_pods                     = "1.2.3.4/32"
+    #module.gke-network.subnets_secondary_ranges[0].*.range_name[0]
+  ip_range_services                 = "1.2.3.4/32"
+    #module.gke-network.subnets_secondary_ranges[0].*.range_name[1]
   enable_private_endpoint           = true
   enable_private_nodes              = true
   master_ipv4_cidr_block            = "172.16.0.16/28"
