78aa1235bc72e868c2554f50910a945ac95e4d2a
Bill Hood <bhood4@gmail.com>
2020-06-26 16:25:11 -0400

    vars for private gke

--- k8s_setup/gcp/variables.tf
+++ k8s_setup/gcp/variables.tf
@@ -1,20 +1,16 @@
 variable "location" {
   type        = string
   description = "Location (region or zone) for resources"
-  default     = "us-west2" 
 }
 
 variable "project" {
   type        = string
   description = "Project were resources are managed"
-  #default     = "bhood-214523"
-  default      = "level-totality-277022" 
 }
 
 variable "cluster_name" {
   type        = string
   description = "cluster name"
-  #default     = "gcp"
   default     = "gcp"
 }
 
@@ -24,8 +20,43 @@ variable "network_name" {
   default     = "default"
 }
 
-variable "credential_file" {
-  type        = string
-  description = "gcp credential file"
-  default    = "~/account.json"
+variable "project_services" {
+  type = list(string)
+  default = [
+    "cloudkms.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "container.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "serviceusage.googleapis.com",
+  ]
+  description = "List of services to enable on the project."
 }
+
+variable "initial_node_count" {
+  type        = number
+  description = "The number of nodes to create in this cluster's default node pool."
+  default     = 0
+}
+
+variable "master_authorized_networks" {
+  type        = list(object({ cidr_block = string, display_name = string }))
+  description = "List of master authorized networks. If none are provided, disallow external access (except the cluster node IPs, which GKE automatically whitelists)."
+  default     = []
+}
+
+variable "master_ipv4_cidr_block" {
+  type        = string
+  description = "(Beta) The IP range in CIDR notation to use for the hosted master network"
+}
+
+variable "ip_range_pods" {
+  description = "The name of the secondary range for the pods"
+}
+
+variable "ip_range_services" {
+  description = "The name of the secondary range for the services"
+}
+
