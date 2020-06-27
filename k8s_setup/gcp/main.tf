2420f8bad9ee49e4df6f51cea2e3741123947b66
Bill Hood <bhood4@gmail.com>
2020-06-27 18:22:32 -0400

    metadata

--- k8s_setup/gcp/main.tf
+++ k8s_setup/gcp/main.tf
@@ -156,6 +156,9 @@ module "gke" {
 #}
 
 resource "kubernetes_cluster_role_binding" "admin" {
+  metadata {
+    name = "admin"
+  }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
