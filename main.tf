063c263f1f2e47d6ad5b9d7363b8cce02ab15873
Bill Hood <bhood4@gmail.com>
2020-06-28 13:38:26 -0400

    ns

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ data "google_container_cluster" "my_cluster" {
   location = data.google_client_config.current.region
 }
 
-#resource "kubernetes_namespace" "namespace" {
-#  count   = length(var.namespaces)
-#  metadata { name = element(var.namespaces, count.index) }
-#}
+resource "kubernetes_namespace" "namespace" {
+  count   = length(var.namespaces)
+  metadata { name = element(var.namespaces, count.index) }
+}
 
 resource "kubernetes_cluster_role_binding" "admin" {
   metadata {
@@ -34,17 +34,16 @@ resource "kubernetes_cluster_role_binding" "admin" {
   }
 }
 
-
 #resource "kubernetes_manifest" "test-configmap" {
 #  provider = kubernetes-alpha
 #  manifest = "$file(../cert-manager-certificaterequests.tf)"
 #}
 
-resource "null_resource" "certmanagerapply" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-  provisioner "local-exec" {
-    command = "kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.14/deploy/manifests/00-crds.yaml && kubectl apply -f k8s_setup/issuer_prod.yaml && kubectl apply -f k8s_setup/issuer_staging.yaml"
-  }
-}
+#resource "null_resource" "certmanagerapply" {
+#  triggers = {
+#    always_run = "${timestamp()}"
+#  }
+#  provisioner "local-exec" {
+#    command = "kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.14/deploy/manifests/00-crds.yaml && kubectl apply -f k8s_setup/issuer_prod.yaml && kubectl apply -f k8s_setup/issuer_staging.yaml"
+#  }
+#}
