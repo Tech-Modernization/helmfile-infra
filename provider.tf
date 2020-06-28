6ad35da4e9bc3060d1b9575b92264f4c0b0cf092
Bill Hood <bhood4@gmail.com>
2020-06-28 13:05:09 -0400

    current

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "google" {
 provider "kubernetes" {
   load_config_file = false
   host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
+  token = data.google_client_config.current.access_token
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
   )
