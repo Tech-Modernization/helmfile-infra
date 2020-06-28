5d766ced6a183b1777553518b30e36bfe76c4287
Bill Hood <bhood4@gmail.com>
2020-06-28 13:26:45 -0400

    no tf providers

--- k8s_setup/gcp/provider.tf
+++ k8s_setup/gcp/provider.tf
@@ -1,15 +1,2 @@
 provider "google" {
 }
-provider "kubernetes" {
-  load_config_file = false
-  host = "https://${module.gke.endpoint}"
-  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-  token = data.google_client_config.current.access_token
-}
-
-#provider "kubernetes-alpha" {
-#  load_config_file = false
-#  host = "https://${module.gke.endpoint}"
-#  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-#  token = data.google_client_config.current.access_token
-#}
