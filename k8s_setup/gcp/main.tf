4178f682dcb4864ee232c9ccbd56d60c2d094b96
Bill Hood <bhood4@gmail.com>
2020-07-16 11:10:50 -0400

    GOOGLE_CREDENTIALS=TODO

--- k8s_setup/gcp/main.tf
+++ k8s_setup/gcp/main.tf
@@ -170,7 +170,7 @@ resource "tfe_variable" "region" {
 
 resource "tfe_variable" "credentials" {
   key          = "GOOGLE_CREDENTIALS"
-  value        = var.TF_VAR_GOOGLE_CREDENTIALS
+  value        = "TODO"
   category     = "env"
   sensitive    = true
   workspace_id = tfe_workspace.project.id
